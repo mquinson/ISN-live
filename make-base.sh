44354b3b3b4dc3c4e9c71d71c48d3270977c032e
Martin Quinson <martin.quinson@loria.fr>
2012-02-08 22:21:36 +0100

    install a backported kernel, and generate the initrd

--- make-base.sh
+++ make-base.sh
@@ -18,5 +18,17 @@ fi
 echo "XXX building the chroot"
 debootstrap $INCLUDEPKG --unpack-tarball=`pwd`/debootstrap.cache.tgz stable chroot $MIRROR
 
+echo "XXX add backports to the apt sources"
+sh -c "echo 'deb http://backports.debian.org/debian-backports squeeze-backports main' >> chroot/etc/apt/sources.list"
+
+echo "XXX install a kernel"
+chroot chroot sh -c "apt-get update; apt-get -t squeeze-backports install --yes linux-image-amd64"
+kver=`ls chroot/lib/modules/ |sort -r|head -n 1`
+
+echo "XXX Generate the initrd"
+cp -r scripts/Mkinitramfs/ chroot/tmp
+chroot chroot sh -c "mkinitramfs -d /tmp/Mkinitramfs/initramfs-tools/ -o /tmp/initrd.gz $kver"
+rm -rf chroot/tmp/Mkinitramfs
+
 echo "XXX Compressing the squash filesystem"
-sudo mksquashfs chroot basesystem.sqh
+mksquashfs chroot basesystem.sqh
