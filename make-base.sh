f9d1e82d789435db77797d867e2bf6082a30daba
Martin Quinson <martin.quinson@loria.fr>
2012-02-13 00:33:29 +0100

    force a fresh copy of the initramfs scripts before generating the initrd file

--- make-base.sh
+++ make-base.sh
@@ -26,6 +26,7 @@ chroot chroot sh -c "apt-get update; apt-get -t squeeze-backports install --yes 
 kver=`ls chroot/lib/modules/ |sort -r|head -n 1`
 
 echo "XXX Generate the initrd"
+rm -rf chroot/etc/ISN-live/initramfs
 mkdir -p chroot/etc/ISN-live/initramfs
 cp -r scripts/Mkinitramfs/initramfs-tools/* chroot/etc/ISN-live/initramfs
 chroot chroot sh -c "mkinitramfs -d /etc/ISN-live/initramfs/ -o /boot/initrd.gz $kver"
