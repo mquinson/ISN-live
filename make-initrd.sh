d3ac5c471865877551e948794af34b621da06b9c
Martin Quinson <martin.quinson@loria.fr>
2012-05-10 23:37:28 +0200

    make the name of chroot configurable

--- make-initrd.sh
+++ make-initrd.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+NAME=clefISN
 
 # Create the initrd within the chroot
 
@@ -12,10 +13,10 @@ fi
 
 set -ex
 
-kver=`ls chroot/lib/modules/ |sort -r|head -n 1`
+kver=`ls $NAME/lib/modules/ |sort -r|head -n 1`
 echo "XXX Generate the initrd (kver: $kver)"
-rm -rf chroot/etc/ISN-live/initramfs
-mkdir -p chroot/etc/ISN-live/initramfs
-cp -r scripts/Mkinitramfs/initramfs-tools/* chroot/etc/ISN-live/initramfs
-chroot chroot sh -c "mkinitramfs -d /etc/ISN-live/initramfs/ -o /boot/initrd.gz $kver"
-cp chroot/boot/initrd.gz .
+rm -rf $NAME/etc/ISN-live/initramfs
+mkdir -p $NAME/etc/ISN-live/initramfs
+cp -r scripts/Mkinitramfs/initramfs-tools/* $NAME/etc/ISN-live/initramfs
+chroot $NAME sh -c "mkinitramfs -d /etc/ISN-live/initramfs/ -o /boot/initrd.gz $kver"
+cp $NAME/boot/initrd.gz .
